ec70c7b2d6df55c0f732edb4360cfbd3935f0b1d
ninakap <1214412@student.roc-nijmegen.nl>
2024-11-22 14:40:13 +0100

    updated

--- src/Mastermindcode2/classes.java
+++ src/Mastermindcode2/classes.java
@@ -8,44 +8,53 @@ public class classes {
 		java.util.Scanner sc = new java.util.Scanner(System.in);
 //dit is de start punt van mijn progamma met de scanner.
 		// TODO Auto-generated method stub
-		int roodPin1 = 1;
-		int geelPin2 = 2;
-		int groenPin3 = 3;
-		int paarsPin4 = 4;
-		int oranjePin5 = 5;
-
-		int zwartPin6 = 6;
-		int witPin7 = 7;
-
-		int specialeVak1 = roodPin1;
-		int specialeVak2 = groenPin3;
-		int specialeVak3 = paarsPin4;
-		int specialeVak4 = geelPin2;
-
+//		int roodPin1 = 1;
+//		int geelPin2 = 2;
+//		int groenPin3 = 3;
+//		int paarsPin4 = 4;
+//		int oranjePin5 = 5;
+        
+	   int[] pins = {1,2,3,4,5};
+				
+				
+//		int zwartPin6 = 6;
+//		int witPin7 = 7;
+		
+       int[]makerPins= {6,7};
+       
+//       int specialeVak1 = roodPin1;
+//		int specialeVak2 = groenPin3;
+//		int specialeVak3 = paarsPin4;
+//		int specialeVak4 = geelPin2;
+//    
+		 int[]geheimeVaken= {pins[0],pins[2],pins[3],pins[1]};
+		 
+		 
 		// dit zijn mijn variablen
 
 		for (int poginen = 0; poginen <= 9; poginen++) {
 			System.out.println("Pogingen:" + ' ' + poginen);
 //poginen
-			int krakerRijVak1 = sc.nextInt();
-			int krakerRijVak2 = sc.nextInt();
-			int krakerRijVak3 = sc.nextInt();
-			int krakerRijVak4 = sc.nextInt();
-			//userinput
-
-			if (krakerRijVak1 == specialeVak1) {
+			int []krakerrijvakken=new int[4];		
+		
+             for(int i=0; i< krakerrijvakken.length; i++)
+             {
+            	 krakerrijvakken[i]=sc.nextInt();
+             }
+             
+			if (krakerrijvakken[0] == pins[0]) {
 
 				System.out.println("zwart");
 
-			} else if (krakerRijVak1 == specialeVak2) {
+			} else if (krakerrijvakken[0] == pins[]) {
 
 				System.out.println("wit");
 
-			} else if (krakerRijVak1 == specialeVak3) {
+			} else if (krakerrijvakken[0] == specialeVak3) {
 
 				System.out.println("wit");
 
-			} else if (krakerRijVak1 == specialeVak4) {
+			} else if(krakerrijvakken[0] == specialeVak4) {
 
 				System.out.println("wit");
 
@@ -55,19 +64,19 @@ public class classes {
 
 			}
 
-			if (krakerRijVak2 == specialeVak2) {
+			if (krakerrijvakken[0] == specialeVak2) {
 
 				System.out.println("zwart");
 
-			} else if (krakerRijVak2 == specialeVak1) {
+			} else if (krakerrijvakken[0] == specialeVak1) {
 
 				System.out.println("wit");
 
-			} else if (krakerRijVak2 == specialeVak3) {
+			} else if(krakerrijvakken[0]== specialeVak3) {
 
 				System.out.println("wit");
 
-			} else if (krakerRijVak2 == specialeVak4) {
+			} else if (krakerrijvakken[0]== specialeVak4) {
 
 				System.out.println("wit");
 
@@ -77,19 +86,19 @@ public class classes {
 
 			}
 
-			if (krakerRijVak3 == specialeVak3) {
+			if (krakerrijvakken[0] == specialeVak3) {
 
 				System.out.println("zwart");
 
-			} else if (krakerRijVak3 == specialeVak2) {
+			} else if (krakerrijvakken[0] == specialeVak2) {
 
 				System.out.println("wit");
 
-			} else if (krakerRijVak3 == specialeVak1) {
+			} else if (krakerrijvakken[0]== specialeVak1) {
 
 				System.out.println("wit");
 
-			} else if (krakerRijVak3 == specialeVak4) {
+			} else if (krakerrijvakken[0] == specialeVak4) {
 
 				System.out.println("wit");
 
@@ -99,19 +108,19 @@ public class classes {
 
 			}
 
-			if (krakerRijVak4 == specialeVak4) {
+			if (krakerrijvakken[0]== specialeVak4) {
 
 				System.out.println("zwart");
 
-			} else if (krakerRijVak4 == specialeVak2) {
+			} else if (krakerrijvakken[0]== specialeVak2) {
 
 				System.out.println("wit");
 
-			} else if (krakerRijVak4 == specialeVak3) {
+			} else if(krakerrijvakken[0]== specialeVak3) {
 
 				System.out.println("wit");
 
-			} else if (krakerRijVak4 == specialeVak1) {
+			} else if (krakerrijvakken[0] == specialeVak1) {
 
 				System.out.println("wit");
 
