9c2ce17bb1f10527596046eb280ecab29c5c0c3d
ninakap <1214412@student.roc-nijmegen.nl>
2025-01-16 14:22:45 +0100

    push

--- src/Mastermindcode2/FunctionBevatWaarde.java
+++ src/Mastermindcode2/FunctionBevatWaarde.java
@@ -1,14 +1,69 @@
 package Mastermindcode2;
 
-public class FunctionBevatWaarde {
+import java.util.Scanner;
 
-    public boolean bevatWaarde(int[] array, int waarde) {
-        for (int i : array) {
-            if (i == waarde) {
+public class FunctionBevatWaarde{
+
+    // Maak een hardcoded geheime code
+    public static int[] maakGeheimeCode() {
+        return new int[]{1, 3, 4, 2}; // Hardcoded geheime code
+    }
+
+    // Vraag een invoer van 4 cijfers van de gebruiker
+    public static int[] vraagInvoer(Scanner sc) {
+        int[] invoer = new int[4];
+        System.out.println("Voer 4 cijfers in (1-5): ");
+        for (int i = 0; i < invoer.length; i++) {
+            invoer[i] = sc.nextInt();
+        }
+        return invoer;
+    }
+
+    // Controleer en geef feedback
+    public static boolean controleerCode(int[] geheimeCode, int[] gok) {
+        boolean gewonnen = true;
+        for (int i = 0; i < geheimeCode.length; i++) {
+            if (gok[i] == geheimeCode[i]) {
+                System.out.println("zwart (juiste positie)");
+            } else if (bevatWaarde(geheimeCode, gok[i])) {
+                System.out.println("wit (verkeerde positie)");
+                gewonnen = false;
+            } else {
+                System.out.println("niet aanwezig");
+                gewonnen = false;
+            }
+        }
+        return gewonnen;
+    }
+
+    // Check of een waarde in de geheime code zit
+    private static boolean bevatWaarde(int[] array, int waarde) {
+        for (int num : array) {
+            if (num == waarde) {
                 return true;
             }
         }
         return false;
+    }
+
+    // Start het spel
+    public static void startSpel(Scanner sc, int[] geheimeCode) {
+        for (int poging = 1; poging <= 10; poging++) {
+            System.out.println("Poging " + poging + ":");
+            
+            int[] gok = vraagInvoer(sc);
+
+            if (controleerCode(geheimeCode, gok)) {
+                System.out.println("Gefeliciteerd! Je hebt de code gekraakt!");
+                return;
+            }
+
+            if (poging == 10) {
+                System.out.println("Helaas! Je hebt de geheime code niet geraden.");
+                System.out.println("De geheime code was: " + 
+                    geheimeCode[0] + " " + geheimeCode[1] + " " + geheimeCode[2] + " " + geheimeCode[3]);
+            }
+        }
     }
 }
 
--- src/Mastermindcode2/classes.java
+++ src/Mastermindcode2/classes.java
@@ -2,52 +2,18 @@ package Mastermindcode2;
 
 import java.util.Scanner;
 
-public class Classes {
+public class Classes{
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        FunctionBevatWaarde functionBevatWaarde = new FunctionBevatWaarde();
 
-        int[] pins = {1, 2, 3, 4, 5};
-        int[] geheimeVakken = {pins[0], pins[2], pins[3], pins[1]}; // {1, 3, 4, 2}
+        // Initialiseer de geheime code
+        int[] geheimeCode = FunctionBevatWaarde.maakGeheimeCode();
 
-        for (int pogingen = 0; pogingen < 9; pogingen++) {
-            System.out.println("Poging: " + (pogingen + 1));
+        // Start het spel
+        FunctionBevatWaarde.startSpel(sc, geheimeCode);
 
-            int[] krakerRijVakken = new int[4];
-            System.out.println("Voer een cijfer in (1-5): ");
-
-            for (int i = 0; i < krakerRijVakken.length; i++) {
-                krakerRijVakken[i] = sc.nextInt();
-            }
-
-            boolean gewonnen = true;
-
-            for (int k = 0; k < krakerRijVakken.length; k++) {
-                if (krakerRijVakken[k] == geheimeVakken[k]) {
-                    System.out.println("zwart");
-                } else if (functionBevatWaarde.bevatWaarde(geheimeVakken, krakerRijVakken[k])) {
-                    System.out.println("wit");
-                    gewonnen = false;
-                } else {
-                    System.out.println("niet aanwezig");
-                    gewonnen = false;
-                }
-            }
-
-            if (gewonnen) {
-                System.out.println("Gefeliciteerd! Je hebt gewonnen!");
-                break;
-            }
-
-            if (pogingen == 8) {
-                System.out.println("Oops, je hebt niet gewonnen. De juiste code was: "
-                        + geheimeVakken[0] + " " + geheimeVakken[1] + " "
-                        + geheimeVakken[2] + " " + geheimeVakken[3]);
-            }
-        }
-
-        
         sc.close();
     }
 }
+
